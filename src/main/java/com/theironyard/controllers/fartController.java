c6ccccc53910b3c1aa4a8a9d4e3327b0726daf2f
Melmo <melodymoselle@gmail.com>
2017-02-01 10:27:13 -0800

    Added paging to home-no-user

--- src/main/java/com/theironyard/controllers/fartController.java
+++ src/main/java/com/theironyard/controllers/fartController.java
@@ -10,10 +10,14 @@ import com.theironyard.repositories.ArtworkRepository;
 import com.theironyard.repositories.UserRepository;
 import com.theironyard.services.ArtsyService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpSession;
@@ -39,16 +43,28 @@ public class fartController {
     ArtsyImageRepository artsyImgRepo;
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String getHome(HttpSession session, Model model){
+    public String getHome(HttpSession session, Model model, @RequestParam(defaultValue = "0") int page){
         if (session.getAttribute(SESSION_USER) != null){
             User user = userRepo.findByUsername(session.getAttribute(SESSION_USER).toString());
             List<Artist> artists = user.getFollowing();
             model.addAttribute("artists", artists);
             return "user-home";
         }
-        List<Artist> artists = artistRepo.findAllPopulatedOrderByFollowedBy();
-        model.addAttribute("artists", artists);
-        return "no-user-home";
+//        List<Artist> artists = artistRepo.findAllPopulatedOrderByFollowedBy();
+//        model.addAttribute("artists", artists);
+        Page<Artwork> artworks = artworkRepo.findAllOrderByLikes(new PageRequest(page, 10));
+        model.addAttribute("artworks", artworks);
+
+        if(artworks.hasPrevious()){
+            model.addAttribute("previous", true);
+            model.addAttribute("prevPageNum", page - 1);
+        }
+        if(artworks.hasNext()){
+            model.addAttribute("next", true);
+            model.addAttribute("nextPageNum", page + 1);
+        }
+
+        return "home-no-user";
     }
 
     @RequestMapping(path = "/discover", method = RequestMethod.GET)
--- src/main/java/com/theironyard/repositories/ArtworkRepository.java
+++ src/main/java/com/theironyard/repositories/ArtworkRepository.java
@@ -2,7 +2,10 @@ package com.theironyard.repositories;
 
 import com.theironyard.entities.Artist;
 import com.theironyard.entities.Artwork;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -10,5 +13,7 @@ public interface ArtworkRepository extends JpaRepository<Artwork, Integer> {
     Artwork findByArtsyArtworkId(String artsyArtworkId);
     List<Artwork> findByArtists(Artist artist);
 
+    @Query("SELECT a  FROM Artwork a LEFT JOIN a.likedBy l GROUP BY a.id ORDER BY count(l) DESC")
+    Page<Artwork> findAllOrderByLikes(Pageable pageable);
 
 }
